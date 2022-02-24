11176115232f00bdc7679aea82a38183faf930bd
valery kalombo <valerykalombo@hotmail.fr>
2022-02-24 17:25:44 +0100

    new coms

--- src/fizz/MainFizzBuzz.java
+++ src/fizz/MainFizzBuzz.java
@@ -4,6 +4,7 @@ public class MainFizzBuzz{
 
 	public static void main(String[] args) {
 		
+		//instaciation de ma classe FizzProblem pour récuperer la methode FindFizzBuzz()
 		FizzProblem fizz = new FizzProblem();
 				
 		fizz.FindFizzBuzz(20);
